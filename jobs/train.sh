2e412a32d1f56711a8e27ea3607a63a98d277fe4
vickie02736 <vickie02736@gmail.com>
2024-06-13 13:41:49 +0100

    0613

--- jobs/train.sh
+++ jobs/train.sh
@@ -14,19 +14,18 @@
 
 # export OMP_NUM_THREADS=1
 
-echo "This script is running on "
-hostname
+# echo "This script is running on "
+# hostname
 
-echo "GPU information" 
-nvidia-smi
+# echo "GPU information" 
+# nvidia-smi
 
 source /home/uceckz0/miniconda3/bin/activate
 conda activate imae
 
 echo $(date +%d-%m-%Y_%H:%M:%S)
 torchrun --nnodes=1 --nproc_per_node=1 ../program/main.py\
-        --epochs 2\
-        --resume-epoch 0\
+        --epochs 600\
         --database shallow_water\
         --model-name cae
 echo $(date +%d-%m-%Y_%H:%M:%S)
